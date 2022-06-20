881bd530eccc26e454a7873c72a7156a49146f6d
qwer-1234-q <503902479monkey@gmail.com>
2022-06-20 21:44:36 +1000

    sorting categories - sylvia

--- backend/src/ingredients_category.py
+++ backend/src/ingredients_category.py
@@ -1,3 +1,58 @@
 """ Sorts list of categories alphabetically and 
     sends data to front end
 """
+
+def sortingCategories(db):
+    """ Sorting categories function to sort the list of categories 
+        by alphabetically.
+
+            Parameters:
+                db: database
+
+            Returns:
+                categories (list): the list of categories after sorting
+    """
+
+    cur = db.cursor()
+    qry = """
+    select * 
+    from categories
+    order by name;
+    """
+    cur.execute(qry)
+    info = cur.fetchone()
+    cur.close()
+
+    if not info:
+        return None
+    else:
+        return info 
+
+
+def sortingIngredients(db):
+    """ Sorting ingredients function to sort the list of ingredients
+        by alphabetically.
+
+            Parameters:
+                db: database
+
+            Returns:
+                ingredients (list): the list of categories after sorting
+    """
+
+    cate = sortingCategories(db)
+    
+    cur = db.cursor()
+    qry = """
+    select * 
+    from categories
+    order by name;
+    """
+    cur.execute(qry)
+    info = cur.fetchone()
+    cur.close()
+
+    if not info:
+        return None
+    else:
+        return info 
