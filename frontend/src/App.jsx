324665dc082ecdb6b3ea9bc6145086398a527d73
Carla Pham <z5256943@ad.unsw.edu.au>
2022-06-22 00:20:54 +1000

    slight update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from './pages'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-      Recipe Recommendation System
-    </div>
+    // <div className="App">
+    //   Recipe Recommendation System !
+    //   { Menu }
+    // </div>
+    <ChakraProvider>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/test' element={<Test/>}/>
+      </Routes>
+    </Router>
+    </ChakraProvider>
   );
+}
+
+function Test() {
+  return (
+    <div>test</div>
+  )
 }
 
 export default App;
---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import './App.css';
+import DropDown from '../components/menu'
+
+function Home() {
+  return (
+    <div className="App">
+      Recipe Recommendation System
+    <DropDown/>
+    </div>
+  );
+}
+
+export default Home;
