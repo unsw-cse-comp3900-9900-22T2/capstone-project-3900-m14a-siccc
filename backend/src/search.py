059d64cff2536c96a7a35acd8c72beaf14f01f49
qwer-1234-q <503902479monkey@gmail.com>
2022-06-21 14:27:42 +1000

    recipe match update  - sylvia

--- backend/src/search.py
+++ backend/src/search.py
@@ -1,15 +1,15 @@
-""" Sends front end a list of recipes that 
-    satisfy the list of ingredients that the user selected.
+
+
+def recipeMatch(db, ingredients_list):
+    """ Sends front end a list of recipes that satisfy the list 
+        of ingredients that the user selected by alphabetically.
 
         Parameters:
             ingredients_list (str): list of ingredients user selected
         
         Return:
             recipe_list (str): list of recipes satisfying the ingredients
-"""
-
-def recipeMatch(db, ingredients_list):
-
+    """
     # [[relevent percetage, recipe information], ..., 
     # [relevent percetage, recipe information]]
     recipes = [] 
@@ -22,7 +22,7 @@ def recipeMatch(db, ingredients_list):
     cur.execute(qry)
     info = cur.fetchone()
     for r in info:
-        ingredients_meal = r[9]
+        ingredients_meal = r[8]
         matching = 0
         for i in ingredients_meal:
             for j in ingredients_list:
@@ -30,11 +30,12 @@ def recipeMatch(db, ingredients_list):
                     matching += 1
                     continue
         matching_percentage = matching / len(ingredients_list)
-        recipes.append([matching_percentage, r])
+        if matching_percentage > 0:
+            recipes.append(r)
     cur.close()
 
     # order from higher percentage to lower percentage
-    recipe_list = sorted(recipes, key=lambda x:x[0], reverse=True) 
+    recipe_list = sorted(recipes, key=lambda x:x[7], reverse=True) 
 
     if len(recipe_list) < 0:
         return None
