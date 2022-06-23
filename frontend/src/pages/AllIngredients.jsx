5c38a89ebdc4a040148e124a9d8572a9d69f122e
Xin Tian Luo <x.t.luo@student.unsw.edu.au>
2022-06-24 01:45:53 +1000

    Added Check box functionality

--- frontend/src/pages/AllIngredients.jsx
+++ frontend/src/pages/AllIngredients.jsx
@@ -6,31 +6,60 @@ const AllIngredients = () => {
   const navigate = useNavigate();
   const [ingredients, setIngredients] = React.useState([]);
   const [recipes, setRecipes] =  React.useState([]);
-  //const [searchIngredient, setSearchIngredient] = React.useState([]);
 
   // Displays all Ingredients
   const viewAllIngredients = async () => {
     try {
-      const ingredientData = await apiFetch('GET', `ingredients/view`, null);
-      setIngredients(ingredientData);
       console.log(ingredients);
-      console.log('here');
+      const ingredientList = [];
+      const ingredientData = await apiFetch('GET', `ingredients/view`, null);
 
+      // Sets a list of dictionary of ingredients
+      console.log("list is 0")
+      for (const ingredient of ingredientData) {
+        const elem = { text: ingredient, check: false };
+        ingredientList.push(elem);
+      }
+      setIngredients(ingredientList);
+      
+      /*if (ingredients.length === 0) {
+      }*/
+      console.log(ingredients)
+      //console.log(ingredients);
+      console.log('here');
+      console.log(ingredientList);
     } catch (err) {
       alert(err.message);
     }
   }
 
+  // Function to set ingrdients selected
+  function toggleIngredients (index) {
+    const newIngredient = [...ingredients];
+    newIngredient[index].check = !ingredients[index].check;
+    setIngredients(newIngredient);
+  }
+
   // Displays all recipes that match
   const recipeMatch = async () => {
     try {
+      const selectedIngredients = [];
+      console.log(ingredients);
+      // Checks if the ingredients are selected and pushes to list
+      for (const ingredient of ingredients) {
+        if (ingredient.check){
+          selectedIngredients.push(ingredient.text);
+        }
+      }
+
+      // Maytches recipe to selected ingredients
       const body = {
-        ingredients: ["bacon"],
+        ingredients: selectedIngredients,
       }
       const recipeData = await apiFetch('POST', `recipe/view`, null, body);
       setRecipes(recipeData.recipes);
       console.log(recipeData.recipes);
-      console.log(recipes.recipes);
+    
       console.log('here');
 
     } catch (err) {
@@ -43,23 +72,28 @@ const AllIngredients = () => {
       <>
         <p>hello</p>
         <button name="allIngredients" onClick={viewAllIngredients}>All Ingredients</button>
-        {ingredients.map((ingredient, idx) => {
-          return (
-            <p key={idx}>
-              {ingredient}
-              <input type="checkbox" id={idx} />
-            </p>
-          )
-        }) }
+        {ingredients.map((ingredient, idx) => (
+          <div key={idx}>
+            <label>
+              {ingredient.text}
+              <input
+                onChange={() => toggleIngredients(idx)}
+                type="checkbox"
+                checked={ingredient.check}
+              />
+            </label>
+          </div>
+        ))}
+
         <button name="search" onClick={recipeMatch}>Search</button>
         {recipes.map((recipe, idx) => {
           return (
-            <p key={idx}>
+            <div key={idx}>
               <div>{recipe.photo}</div>
               <h1 onClick={() => navigate(`/recipe-details/${recipe.recipeID}`)}>{recipe.title}</h1>
               <p>ingredients: {recipe.ingredients}</p>
               <hr></hr>
-            </p>
+            </div>
           )
         }) }
 
