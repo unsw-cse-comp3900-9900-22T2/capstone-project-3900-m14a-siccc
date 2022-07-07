225a556f4e8bdab8563c42cceee4dbccdea9c990
qwer-1234-q <503902479monkey@gmail.com>
2022-07-07 13:50:21 +1000

    updated calories recipes and tests 3nd - Sylvia

--- backend/tests/calories_recipes_test.py
+++ backend/tests/calories_recipes_test.py
@@ -3,12 +3,12 @@ import pytest
 
 
 def test_simple_calories_match():
-    calories = 100
-    assert len(getCaloriesRecipes(calories)) == 1
+    calories = 200
+    assert len(getCaloriesRecipes(calories)) == 2
 
 def test_simple_calories_ingredients_match():
     ingredients = ['olive oil', 'bacon', 'eggs']
-    calories = 100
+    calories = 200
     assert len(getCaloriesRecipesWithIngredients(calories, ingredients)) == 1
     
 def test_no_calories_match():
@@ -21,19 +21,19 @@ def test_no_calories_ingredients_match():
     assert len(getCaloriesRecipesWithIngredients(calories, ingredients)) == 0
 
 def test_minimal_calories_match():
-    calories = 100
+    calories = 99
     assert len(getCaloriesRecipes(calories)) == 0
 
 def test_minimal_calories_ingredients_match():
     ingredients = ['bacon']
-    calories = 100
+    calories = 99
     assert len(getCaloriesRecipesWithIngredients(calories, ingredients)) == 0
     
 def test_multiple_calories_match():
     calories = 10000
-    assert len(getCaloriesRecipes(calories)) == 2
+    assert len(getCaloriesRecipes(calories)) == 8
 
 def test_multiple_calories_ingredients_match():
     ingredients = ['bacon', 'eggs', 'flour', 'olive oil', 'thyme', 'avocado']
-    calories = 100
-    assert len(getCaloriesRecipesWithIngredients(calories, ingredients)) == 2
+    calories = 10000
+    assert len(getCaloriesRecipesWithIngredients(calories, ingredients)) == 6
