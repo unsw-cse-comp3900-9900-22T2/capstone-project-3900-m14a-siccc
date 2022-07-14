48dd054aae5fd9dc4a85f74b228294e2ecc8b9c5
qwer-1234-q <503902479monkey@gmail.com>
2022-07-14 16:43:57 +1000

    calories ingredients meal type - Sylvia

--- backend/tests/calories_recipes_test.py
+++ backend/tests/calories_recipes_test.py
@@ -1,4 +1,4 @@
-from src.calories_recipes import getCaloriesRecipes, getCaloriesRecipesWithIngredients, calorieCalculation
+from src.calories_recipes import getCaloriesRecipes, getCaloriesRecipesWithIngredients, calorieCalculation, getRecipesWithCaloriesIngredientsMealType
 import pytest
 
 
@@ -10,6 +10,12 @@ def test_simple_calories_ingredients_match():
     ingredients = ['olive oil', 'bacon', 'egg']
     calories = 700
     assert len(getCaloriesRecipesWithIngredients(calories, ingredients)) == 1
+
+def test_simple_calories_ingredients_mealType_match():
+    ingredients = ['olive oil', 'bacon', 'egg']
+    calories = 700
+    mealType = "breakfast"
+    assert len(getRecipesWithCaloriesIngredientsMealType(calories, ingredients, mealType)) == 1
     
 def test_no_calories_match():
     calories = 10
@@ -20,6 +26,12 @@ def test_no_calories_ingredients_match():
     calories = 10
     assert len(getCaloriesRecipesWithIngredients(calories, ingredients)) == 0
 
+def test_no_calories_ingredients_mealType_match():
+    ingredients = ['olive oil', 'bacon', 'egg']
+    calories = 700
+    mealType = "dinner"
+    assert len(getRecipesWithCaloriesIngredientsMealType(calories, ingredients, mealType)) == 0
+
 def test_minimal_calories_match():
     calories = 99
     assert len(getCaloriesRecipes(calories)) == 1
@@ -28,7 +40,13 @@ def test_minimal_calories_ingredients_match():
     ingredients = ['bacon']
     calories = 99
     assert len(getCaloriesRecipesWithIngredients(calories, ingredients)) == 0
-    
+
+def test_minimal_calories_ingredients_mealType_match():
+    ingredients = ['bacon']
+    calories = 99
+    mealType = "dinner"
+    assert len(getRecipesWithCaloriesIngredientsMealType(calories, ingredients, mealType)) == 0
+
 def test_multiple_calories_match():
     calories = 10000
     assert len(getCaloriesRecipes(calories)) == 9
@@ -41,3 +59,10 @@ def test_multiple_calories_ingredients_match():
 def test_calorieCalc_normal():
     ingredientsDict = {"olive oil": "3g",  "bacon": "100g", "egg": 2}
     assert calorieCalculation(ingredientsDict) == 660
+
+
+def test_multiple_calories_ingredients_mealType_match():
+    ingredients = ['bacon', 'egg', 'flour', 'olive oil', 'thyme', 'avocado']
+    calories = 10000
+    mealType = "breakfast"
+    assert len(getRecipesWithCaloriesIngredientsMealType(calories, ingredients, mealType)) == 1
