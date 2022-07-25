56fb98e4bc05b27baaa57797456435cc1cc9b244
Kristofa <z5310212@ad.unsw.edu.au>
2022-07-25 20:09:47 +1000

    adding route for blacklist

--- backend/src/server.py
+++ backend/src/server.py
@@ -4,12 +4,13 @@ from flask_cors import CORS
 #from matplotlib.pyplot import get
 from src.calories_recipes import getCaloriesRecipesWithIngredients, getRecipesWithCaloriesIngredientsMealType
 from src.error import InputError
-from src.recipe import recipeMatch, recipeDetails
+from src.recipe import recipeMatch, recipeDetails, recipeMatchwithBlacklist
 from src.ingredients import IngredientsViewAll
 from src.ingredients_category import sortIngredientsInCategories
 from src import config
 from src.recipeContributor import insertRecipe, getNoRecipeMatchList, addFrequency
 from src.mealType import getMealType
+
 
 def defaultHandler(err):
     response = err.get_response()
@@ -22,8 +23,10 @@ def defaultHandler(err):
     response.content_type = 'application/json'
     return response
 
-APP = Flask(__name__, static_url_path = '/static')
+
+APP = Flask(__name__, static_url_path='/static')
 CORS(APP)
+
 
 @APP.route("/recipe/view", methods=['POST'])
 def recipeMatchFlask():
@@ -36,20 +39,24 @@ def recipeMatchFlask():
         'recipes': info
     })
 
+
 @APP.route("/recipe/details/<page_id>", methods=['GET'])
 def recipeDetailsFlask(page_id):
     info = recipeDetails(page_id)
     return dumps(info)
-    
+
+
 @APP.route("/ingredients/view", methods=['GET'])
 def IngredientsView():
     info = IngredientsViewAll()
     return dumps(info)
 
+
 @APP.route("/ingredients/categories", methods=['GET'])
 def ingredientsCategories():
     info = sortIngredientsInCategories()
     return dumps(info)
+
 
 @APP.route("/insert/recipe", methods=['POST'])
 def insertRecipeFlask():
@@ -58,11 +65,13 @@ def insertRecipeFlask():
     insertRecipe(recipeDetails)
     return dumps(1)
 
+
 @APP.route("/no/recipe/match", methods=['GET'])
 def getNoRecipeMatchFlask():
     return dumps(getNoRecipeMatchList())
 
-@APP.route("/recipe/calorie/view", methods = ['POST'])
+
+@APP.route("/recipe/calorie/view", methods=['POST'])
 def recipeMatchCalorieFlask():
     temp = request.get_json()
     ingredients = temp['ingredients']
@@ -71,8 +80,9 @@ def recipeMatchCalorieFlask():
     return dumps({
         'recipes': info
     })
-    
-@APP.route("/recipe/mealtype/view", methods = ['POST'])
+
+
+@APP.route("/recipe/mealtype/view", methods=['POST'])
 def recipeMatchMealTypeFlask():
     temp = request.get_json()
     ingredients = temp['ingredients']
@@ -81,20 +91,44 @@ def recipeMatchMealTypeFlask():
     return dumps({
         'recipes': info
     })
-    
-@APP.route("/recipe/calorie/mealtype/view", methods = ['POST'])
+
+
+@APP.route("/recipe/calorie/mealtype/view", methods=['POST'])
 def recipeMatchMealTypeCalorieFlask():
     temp = request.get_json()
     ingredients = temp['ingredients']
     mealType = temp['mealType']
     calories = temp['calories']
-    info = getRecipesWithCaloriesIngredientsMealType(calories, ingredients, mealType)
+    info = getRecipesWithCaloriesIngredientsMealType(
+        calories, ingredients, mealType)
     return dumps({
         'recipes': info
     })
-    
+
+
+@APP.route("/recipe/blacklistView", methods=['POST'])
+def recipeMatchFlask():
+    temp = request.get_json()
+    ingredients = temp['ingredients']
+    blacklist = temp['blacklist']
+    info = recipeMatchwithBlacklist(ingredients, blacklist)
+    if len(info) == 0:
+        addFrequency(ingredients)
+    return dumps({
+        'recipes': info
+    })
+
+@APP.route("/recipe/view", methods=['POST'])
+def recipeMatchFlask():
+    temp = request.get_json()
+    ingredients = temp['ingredients']
+    info = recipeMatch(ingredients)
+    if len(info) == 0:
+        addFrequency(ingredients)
+    return dumps({
+        'recipes': info
+    })
+
+
 if __name__ == "__main__":
     APP.run(port=config.port)
-
-
-
