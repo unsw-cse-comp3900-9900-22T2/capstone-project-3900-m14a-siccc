0cf2b6625abfc5b469ef5cd9f08712f82438ac84
qwer-1234-q <503902479monkey@gmail.com>
2022-07-30 15:44:23 +1000

    ingredients and meal type cleaning - sylvia

--- backend/src/mealType.py
+++ backend/src/mealType.py
@@ -1,25 +1,20 @@
-from src.recipe import getFilteredRecipes, recipeMatch
+from src.recipe import recipeMatch
 
 
 def getMealType(meal, ingredientsList, blacklist):
     """ Select the meal type after the recipe match. 
 
             Parameters:
-                meal: the name of meal type
-                ingredientsList: the list of ingredientsList
+                meal (str): the name of meal type
+                ingredientsList (list): the list of ingredientsList
         
             Returns:
-                recipleTypeList: list of all recipes ingredients
+                recipleTypeList (list): list of all recipes ingredients
     """
     recipeList = recipeMatch(ingredientsList, blacklist)
-
     recipeTypeList = []
-
     for recipe in recipeList:
         if recipe["mealType"] == meal:
             recipeTypeList.append(recipe)
 
     return recipeTypeList
-
-
-
