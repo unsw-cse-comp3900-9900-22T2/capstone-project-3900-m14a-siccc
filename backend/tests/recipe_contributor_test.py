605b47ed9e7117dde9a9fce73d2032c9dc9e588e
qwer-1234-q <503902479monkey@gmail.com>
2022-07-31 14:16:04 +1000

    fix recipe_test and delete recipe_contributor_test

--- backend/tests/recipe_contributor_test.py
+++
@@ -1,7 +0,0 @@
-from src.recipeContributor import getNoRecipeMatchList
-import pytest
-
-def test_frequency():
-    ingredients = ['thyme', 'eggs']
-    noMatch = getNoRecipeMatchList()
-    assert noMatch[0] == ingredients
